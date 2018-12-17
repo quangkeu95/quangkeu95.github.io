a576a64a5c50d6bcb29e018c31b2cf3dbb67aa35
Nguyen Le Quang <quangnl@vnpt-technology.vn>
2018-12-17 17:34:56 +0700

    Updates

---
+++ precache-manifest.d6799e5262800e75902bc269600fae4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quangkeu95.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9bbb9891dbc74792bc62",
+    "url": "/quangkeu95.github.io/static/js/runtime~main.9bbb9891.js"
+  },
+  {
+    "revision": "93583828e0b007a1bf43",
+    "url": "/quangkeu95.github.io/static/js/main.93583828.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/quangkeu95.github.io/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "93583828e0b007a1bf43",
+    "url": "/quangkeu95.github.io/static/css/main.a963596a.chunk.css"
+  },
+  {
+    "revision": "fe280811472eea0def9cb729e6be7fde",
+    "url": "/quangkeu95.github.io/index.html"
+  }
+];
